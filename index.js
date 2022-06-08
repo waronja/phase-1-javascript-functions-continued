099c67d2c7cdc182bd496bf86817be6cfdf6c4e5
unknown <crazsich@gmail.com>
2022-06-08 23:26:28 +0300

    function assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun () {
+  const lastpart = "bathe my dog";
+    console.log ("This Saturday, I want to " + lastpart + "!") ;
+
+    return saturdayFun ();
+}
+
+function mondayWork () {
+  const lastpart = "work from home";
+    console.log ("This Monday, I will " + lastpart + ".") ;
+
+    return mondayWork ();
+}
+
+
+function wrapAdjective (delimiter) {
+  return function (adjective) {
+    return "You are " + delimiter + adjective + delimiter + "!";
+  }
+}
+
